d002c35d469e0c9318d0dcaf1311b552697a69f0
Phil Syme <phil.syme@gmail.com>
2019-09-04 14:18:44 +0000

    first cut at CJavaScriptArray implementation

--- src/Wrapper.cpp
+++ src/Wrapper.cpp
@@ -1261,15 +1261,333 @@ py::object CJavascriptObject::Wrap(CJavascriptObject *obj)
 }
 
 // =====================================================================
-// CJavascriptObject End 
+// CJavascriptObject Untested
 // =====================================================================
 
 void CJavascriptArray::LazyConstructor(void)
 {
-  //Stubbed
+  if (!m_obj.IsEmpty()) return;
+
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::HandleScope handle_scope(isolate);
+  v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+  v8::Handle<v8::Array> array;
+
+  if (m_items.is_none())
+  {
+    array = v8::Array::New(isolate, m_size);
+  }
+#if PY_MAJOR_VERSION < 3
+  else if (PyInt_CheckExact(m_items.ptr()))
+  {
+    m_size = PyInt_AS_LONG(m_items.ptr());
+    array = v8::Array::New(isolate, m_size);
+  }
+#endif
+  else if (PyLong_CheckExact(m_items.ptr()))
+  {
+    m_size = PyLong_AsLong(m_items.ptr());
+    array = v8::Array::New(isolate, m_size);
+  }
+  else if (PyList_Check(m_items.ptr()))
+  {
+    m_size = PyList_GET_SIZE(m_items.ptr());
+    array = v8::Array::New(isolate, m_size);
+
+    for (Py_ssize_t i=0; i< (Py_ssize_t) m_size; i++)
+    {
+      array->Set(context, v8::Uint32::New(isolate, i), CPythonObject::Wrap(py::object(py::handle<>(py::borrowed(PyList_GET_ITEM(m_items.ptr(), i))))));
+    }
+  }
+  else if (PyTuple_Check(m_items.ptr()))
+  {
+    m_size = PyTuple_GET_SIZE(m_items.ptr());
+    array = v8::Array::New(isolate, m_size);
+
+    for (Py_ssize_t i=0; i< (Py_ssize_t) m_size; i++)
+    {
+      array->Set(context, v8::Uint32::New(isolate, i), CPythonObject::Wrap(py::object(py::handle<>(py::borrowed(PyTuple_GET_ITEM(m_items.ptr(), i))))));
+    }
+  }
+  else if (PyGen_Check(m_items.ptr()))
+  {
+    array = v8::Array::New(isolate);
+
+    py::object iter(py::handle<>(::PyObject_GetIter(m_items.ptr())));
+
+    m_size = 0;
+    PyObject *item = NULL;
+
+    while (NULL != (item = ::PyIter_Next(iter.ptr())))
+    {
+      array->Set(context, v8::Uint32::New(isolate, m_size++), CPythonObject::Wrap(py::object(py::handle<>(py::borrowed(item)))));
+    }
+  }
+
+  m_obj.Reset(isolate, array);
+}
+// =====================================================================
+// CJavascriptObject End 
+// =====================================================================
+
+
+// =====================================================================
+// CJavascriptArray Begin untested 
+// =====================================================================
+
+size_t CJavascriptArray::Length(void)
+{
+  CHECK_V8_CONTEXT();
+
+  LazyConstructor();
+
+  v8::HandleScope handle_scope(v8::Isolate::GetCurrent());
+
+  return v8::Handle<v8::Array>::Cast(Object())->Length();
+}
+
+py::object CJavascriptArray::GetItem(py::object key)
+{
+#ifdef SUPPORT_PROBES
+  if (WRAPPER_JS_ARRAY_GETITEM_ENABLED()) {
+    WRAPPER_JS_ARRAY_GETITEM(&m_obj, key.ptr());
+  }
+#endif
+
+  CHECK_V8_CONTEXT();
+
+  LazyConstructor();
+
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::HandleScope handle_scope(isolate);
+  v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+  v8::TryCatch try_catch(isolate);
+
+  if (PySlice_Check(key.ptr()))
+  {
+    Py_ssize_t arrayLen = v8::Handle<v8::Array>::Cast(Object())->Length();
+    Py_ssize_t start, stop, step, sliceLen;
+
+    if (0 == ::PySlice_GetIndicesEx(PySlice_Cast(key.ptr()), arrayLen, &start, &stop, &step, &sliceLen))
+    {
+      py::list slice;
+
+      for (Py_ssize_t idx=start; idx<stop; idx+=step)
+      {
+        v8::Handle<v8::Value> value = Object()->Get(context, v8::Integer::New(isolate, (uint32_t) idx)).ToLocalChecked();
+
+        if (value.IsEmpty()) CJavascriptException::ThrowIf(isolate, try_catch);
+
+        slice.append(CJavascriptObject::Wrap(value, Object()));
+      }
+
+      return slice;
+    }
+  }
+  else if (PyInt_Check(key.ptr()) || PyLong_Check(key.ptr()))
+  {
+    uint32_t idx = PyInt_Check(key.ptr()) ? (uint32_t) ::PyInt_AsUnsignedLongMask(key.ptr()) : (uint32_t) ::PyLong_AsUnsignedLong(key.ptr());
+
+    if (!Object()->Has(context,idx).ToChecked()) return py::object();
+
+    v8::Handle<v8::Value> value = Object()->Get(context, v8::Integer::New(isolate, idx)).ToLocalChecked();
+
+    if (value.IsEmpty()) CJavascriptException::ThrowIf(isolate, try_catch);
+
+    return CJavascriptObject::Wrap(value, Object());
+  }
+
+  throw CJavascriptException("list indices must be integers", ::PyExc_TypeError);
+}
+
+py::object CJavascriptArray::SetItem(py::object key, py::object value)
+{
+/* TODO port me
+#ifdef SUPPORT_PROBES
+  if (WRAPPER_JS_ARRAY_SETITEM_ENABLED()) {
+    WRAPPER_JS_ARRAY_SETITEM(&m_obj, key.ptr(), value.ptr());
+  }
+#endif
+*/
+
+  CHECK_V8_CONTEXT();
+
+  LazyConstructor();
+
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::HandleScope handle_scope(isolate);
+  v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+  v8::TryCatch try_catch(isolate);
+
+  if (PySlice_Check(key.ptr()))
+  {
+    PyObject *values = ::PySequence_Fast(value.ptr(), "can only assign an iterable");
+
+    if (values)
+    {
+      Py_ssize_t itemSize = PySequence_Fast_GET_SIZE(value.ptr());
+      PyObject **items = PySequence_Fast_ITEMS(value.ptr());
+
+      Py_ssize_t arrayLen = v8::Handle<v8::Array>::Cast(Object())->Length();
+      Py_ssize_t start, stop, step, sliceLen;
+
+      if (0 == ::PySlice_GetIndicesEx(PySlice_Cast(key.ptr()), arrayLen, &start, &stop, &step, &sliceLen))
+      {
+        /* TODO port me BUGBUG
+         * This will require some work since we are trying to avoid the
+         * internal interface.  But, it may be needed.
+        if (itemSize != sliceLen)
+        {
+          v8i::Handle<v8i::JSArray> array = v8::Utils::OpenHandle(*Object());
+
+          array->set_length(v8i::Smi::FromInt(arrayLen - sliceLen + itemSize));
+
+          if (itemSize < sliceLen)
+          {
+            Py_ssize_t diff = sliceLen - itemSize;
+
+            for (Py_ssize_t idx=start+itemSize; idx<arrayLen-diff; idx++)
+            {
+              Object()->Set(idx, Object()->Get((uint32_t) (idx + diff)));
+            }
+            for (Py_ssize_t idx=arrayLen-1; idx >arrayLen-diff-1; idx--)
+            {
+              Object()->Delete((uint32_t) idx);
+            }
+          }
+          else if (itemSize > sliceLen)
+          {
+            Py_ssize_t diff = itemSize - sliceLen;
+
+            for (Py_ssize_t idx=arrayLen+diff-1; idx>stop-1; idx--)
+            {
+              Object()->Set(idx, Object()->Get((uint32_t) (idx - diff)));
+            }
+          }
+        }
+        */
+
+        for (Py_ssize_t idx=0; idx<itemSize; idx++)
+        {
+          Object()->Set(context, (uint32_t) (start + idx), CPythonObject::Wrap(py::object(py::handle<>(py::borrowed(items[idx])))));
+        }
+      }
+    }
+  }
+  else if (PyInt_Check(key.ptr()) || PyLong_Check(key.ptr()))
+  {
+    uint32_t idx = PyInt_Check(key.ptr()) ? (uint32_t) ::PyInt_AsUnsignedLongMask(key.ptr()) : (uint32_t) ::PyLong_AsUnsignedLong(key.ptr());
+
+    if (!Object()->Set(context, v8::Integer::New(v8::Isolate::GetCurrent(), idx), CPythonObject::Wrap(value)).ToChecked())
+      CJavascriptException::ThrowIf(v8::Isolate::GetCurrent(), try_catch);
+  }
+
+  return value;
+}
+
+py::object CJavascriptArray::DelItem(py::object key)
+{
+/* TODO port me
+#ifdef SUPPORT_PROBES
+  if (WRAPPER_JS_ARRAY_DELITEM_ENABLED()) {
+    WRAPPER_JS_ARRAY_DELITEM(&m_obj, key.ptr());
+  }
+#endif
+*/
+
+  CHECK_V8_CONTEXT();
+
+  LazyConstructor();
+
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::HandleScope handle_scope(isolate);
+  v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+  v8::TryCatch try_catch(isolate);
+
+  if (PySlice_Check(key.ptr()))
+  {
+    Py_ssize_t arrayLen = v8::Handle<v8::Array>::Cast(Object())->Length();
+    Py_ssize_t start, stop, step, sliceLen;
+
+    if (0 == ::PySlice_GetIndicesEx(PySlice_Cast(key.ptr()), arrayLen, &start, &stop, &step, &sliceLen))
+    {
+      for (Py_ssize_t idx=stop; idx<arrayLen; idx++)
+      {
+        Object()->Set(context, (uint32_t) (idx - sliceLen), Object()->Get(context, (uint32_t) idx).ToLocalChecked());
+      }
+
+      for (Py_ssize_t idx=arrayLen-1; idx>arrayLen-sliceLen-1; idx--)
+      {
+        Object()->Delete(context, (uint32_t)idx);
+      }
+
+      /* TODO port me BUGBUG
+       * Look into if we have to use the private api
+      v8i::Handle<v8i::JSArray> array = v8::Utils::OpenHandle(*Object());
+
+      array->set_length(v8i::Smi::FromInt(arrayLen - sliceLen));
+      */
+    }
+
+    return py::object();
+  }
+  else if (PyInt_Check(key.ptr()) || PyLong_Check(key.ptr()))
+  {
+    uint32_t idx = PyInt_Check(key.ptr()) ? (uint32_t) ::PyInt_AsUnsignedLongMask(key.ptr()) : (uint32_t) ::PyLong_AsUnsignedLong(key.ptr());
+
+    py::object value;
+
+    if (Object()->Has(context, idx).ToChecked())
+      value = CJavascriptObject::Wrap(Object()->Get(context, v8::Integer::New(v8::Isolate::GetCurrent(), idx)).ToLocalChecked(), Object());
+
+    if (!Object()->Delete(context, idx).ToChecked())
+      CJavascriptException::ThrowIf(v8::Isolate::GetCurrent(), try_catch);
+
+    return value;
+  }
+
+  throw CJavascriptException("list indices must be integers", ::PyExc_TypeError);
+}
+
+bool CJavascriptArray::Contains(py::object item)
+{
+  CHECK_V8_CONTEXT();
+
+  LazyConstructor();
+
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::HandleScope handle_scope(isolate);
+  v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+  v8::TryCatch try_catch(isolate);
+
+  for (size_t i=0; i<Length(); i++)
+  {
+    if (Object()->Has(context, i).ToChecked())
+    {
+      v8::Handle<v8::Value> value = Object()->Get(context, v8::Integer::New(isolate, i)).ToLocalChecked();
+
+      if (try_catch.HasCaught()) CJavascriptException::ThrowIf(v8::Isolate::GetCurrent(), try_catch);
+
+      if (item == CJavascriptObject::Wrap(value, Object()))
+      {
+        return true;
+      }
+    }
+  }
+
+  if (try_catch.HasCaught()) CJavascriptException::ThrowIf(v8::Isolate::GetCurrent(), try_catch);
+
+  return false;
 }
 
 
+
+/******** Testing / Learning *******/
 static v8::Isolate* GetNewIsolate()
 {
   v8::Isolate::CreateParams create_params;
