3356321825f64cd3d801c37cd8a7071e739925be
Phil Syme <phil.syme@gmail.com>
2019-09-16 16:07:35 +0000

    adds expose port

--- src/Context.cpp
+++ src/Context.cpp
@@ -35,7 +35,75 @@ CJavascriptStackTracePtr CIsolate::GetCurrentStackTrace(int frame_limit,
 
 void CContext::Expose(void)
 {
-//TODO port me
+  py::class_<CIsolate, boost::noncopyable>("JSIsolate", "JSIsolate is an isolated instance of the V8 engine.", py::no_init)
+    .def(py::init<bool>((py::arg("owner") = false)))
+
+    .add_static_property("current", &CIsolate::GetCurrent,
+                         "Returns the entered isolate for the current thread or NULL in case there is no current isolate.")
+
+    .add_property("locked", &CIsolate::IsLocked)
+
+    .def("GetCurrentStackTrace", &CIsolate::GetCurrentStackTrace)
+
+    .def("enter", &CIsolate::Enter,
+         "Sets this isolate as the entered one for the current thread. "
+         "Saves the previously entered one (if any), so that it can be "
+         "restored when exiting.  Re-entering an isolate is allowed.")
+
+    .def("leave", &CIsolate::Leave,
+         "Exits this isolate by restoring the previously entered one in the current thread. "
+         "The isolate may still stay the same, if it was entered more than once.")
+    ;
+
+  py::class_<CContext, boost::noncopyable>("JSContext", "JSContext is an execution context.", py::no_init)
+    .def(py::init<const CContext&>("create a new context base on a exists context"))
+    .def(py::init<py::object, py::list>((py::arg("global") = py::object(),
+                                         py::arg("extensions") = py::list()),
+                                        "create a new context base on global object"))
+
+    .add_property("securityToken", &CContext::GetSecurityToken, &CContext::SetSecurityToken)
+
+    .add_property("locals", &CContext::GetGlobal, "Local variables within context")
+
+    .add_static_property("entered", &CContext::GetEntered,
+                         "The last entered context.")
+    .add_static_property("current", &CContext::GetCurrent,
+                         "The context that is on the top of the stack.")
+    .add_static_property("calling", &CContext::GetCalling,
+                         "The context of the calling JavaScript code.")
+    .add_static_property("inContext", &CContext::InContext,
+                         "Returns true if V8 has a current context.")
+
+    .add_property("hasOutOfMemoryException", &CContext::HasOutOfMemoryException)
+
+    .def("eval", &CContext::Evaluate, (py::arg("source"),
+                                       py::arg("name") = std::string(),
+                                       py::arg("line") = -1,
+                                       py::arg("col") = -1,
+                                       py::arg("precompiled") = py::object()))
+    .def("eval", &CContext::EvaluateW, (py::arg("source"),
+                                        py::arg("name") = std::wstring(),
+                                        py::arg("line") = -1,
+                                        py::arg("col") = -1,
+                                        py::arg("precompiled") = py::object()))
+
+    .def("enter", &CContext::Enter, "Enter this context. "
+         "After entering a context, all code compiled and "
+         "run is compiled and run in this context.")
+    .def("leave", &CContext::Leave, "Exit this context. "
+         "Exiting the current context restores the context "
+         "that was in place when entering the current context.")
+
+    .def("__nonzero__", &CContext::IsEntered, "the context has been entered.")
+    ;
+
+  py::objects::class_value_wrapper<boost::shared_ptr<CIsolate>,
+    py::objects::make_ptr_instance<CIsolate,
+    py::objects::pointer_holder<boost::shared_ptr<CIsolate>,CIsolate> > >();
+
+  py::objects::class_value_wrapper<boost::shared_ptr<CContext>,
+    py::objects::make_ptr_instance<CContext,
+    py::objects::pointer_holder<boost::shared_ptr<CContext>,CContext> > >();
 }
 
 py::object CIsolate::GetCurrent(void)
