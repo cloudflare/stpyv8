67ead197a0396ea2ee4821941e81d779fa4def3b
Angelo Dell'Aera <buffer@antifork.org>
2019-10-01 08:54:12 +0000

    CLocker/CUnlocker support

---
+++ src/Locker.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Exception.h"
+#include "Context.h"
+#include "Utils.h"
+
+
+class CLocker
+{
+  static bool s_preemption;
+
+  std::unique_ptr<v8::Locker> m_locker;
+  CIsolatePtr m_isolate;
+public:
+  CLocker() {}
+  CLocker(CIsolatePtr isolate) : m_isolate(isolate) {}
+  bool entered(void) { return NULL != m_locker.get(); }
+
+  void enter(void);
+  void leave(void);
+
+  static bool IsLocked();
+
+  static void Expose(void);
+};
+
+
+class CUnlocker
+{
+  std::unique_ptr<v8::Unlocker> m_unlocker;
+public:
+  bool entered(void) { return NULL != m_unlocker.get(); }
+
+  void enter(void)
+  {
+    Py_BEGIN_ALLOW_THREADS
+
+    m_unlocker.reset(new v8::Unlocker(v8::Isolate::GetCurrent()));
+
+    Py_END_ALLOW_THREADS
+  }
+
+  void leave(void)
+  {
+    Py_BEGIN_ALLOW_THREADS
+
+    m_unlocker.reset();
+
+    Py_END_ALLOW_THREADS
+  }
+};
