ff84165cfb6ea2f7ae5af1940004cd14da9505a4
Angelo Dell'Aera <angelo@area1security.com>
2020-07-06 11:00:32 +0200

    Minor MacOS X platform improvement

--- src/Config.h
+++ src/Config.h
@@ -8,9 +8,12 @@
 
 // ICU data file
 #define ICU_DATA_UNIX "/usr/share/stpyv8/icudtl.dat"
+#define ICU_DATA_OSX  "/Library/Application Support/STPyV8/icudtl.dat"
 
-#if defined(__linux) || defined(__APPLE)
-#  define ICU_DATA ICU_DATA_UNIX;
+#if defined(__linux)
+#  define ICU_DATA ICU_DATA_UNIX
+#elif defined(__APPLE)
+#  define ICU_DATA ICU_DATA_OSX
 #else
-#  define ICU_DATA nullptr;
+#  define ICU_DATA nullptr
 #endif
