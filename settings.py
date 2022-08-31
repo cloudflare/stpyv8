e8dc45a60040e0bbb40713d55dbf45c5767e3c3f
drunkdream <drunkdream@qq.com>
2022-08-31 22:56:02 +0800

    Support static link boost

--- settings.py
+++ settings.py
@@ -3,85 +3,101 @@ import os
 import platform
 
 STPYV8_HOME = os.path.dirname(os.path.realpath(__file__))
-DEPOT_HOME  = os.environ.get('DEPOT_HOME', os.path.join(STPYV8_HOME, 'depot_tools'))
-V8_HOME     = os.environ.get('V8_HOME', os.path.join(STPYV8_HOME, 'v8'))
+DEPOT_HOME = os.environ.get("DEPOT_HOME", os.path.join(STPYV8_HOME, "depot_tools"))
+V8_HOME = os.environ.get("V8_HOME", os.path.join(STPYV8_HOME, "v8"))
+BOOST_HOME = os.environ.get("BOOST_HOME", os.path.join(STPYV8_HOME, "boost"))
 
-V8_GIT_URL        = "https://chromium.googlesource.com/v8/v8.git"
+V8_GIT_URL = "https://chromium.googlesource.com/v8/v8.git"
 V8_GIT_TAG_STABLE = "10.5.218.7"
 V8_GIT_TAG_MASTER = "master"
-V8_GIT_TAG        = V8_GIT_TAG_STABLE
-DEPOT_GIT_URL     = "https://chromium.googlesource.com/chromium/tools/depot_tools.git"
+V8_GIT_TAG = V8_GIT_TAG_STABLE
+DEPOT_GIT_URL = "https://chromium.googlesource.com/chromium/tools/depot_tools.git"
+BOOST_VERSION = "1.67.0"
+BOOST_ROOT = os.path.join(BOOST_HOME, "boost_%s" % BOOST_VERSION.replace(".", "_"))
 
 STPYV8_VERSION = V8_GIT_TAG_STABLE
 
-v8_deps_linux = os.environ.get('V8_DEPS_LINUX', '1') in ('1', )
+v8_deps_linux = os.environ.get("V8_DEPS_LINUX", "1") in ("1",)
 
 ICU_DATA_FOLDER_UNIX = "/usr/share/stpyv8"
-ICU_DATA_FOLDER_OSX  = "/Library/Application Support/STPyV8/"
+ICU_DATA_FOLDER_OSX = "/Library/Application Support/STPyV8/"
 
-if os.name in ("posix", ):
-    icu_data_folder = ICU_DATA_FOLDER_OSX if sys.platform in ("darwin", ) else ICU_DATA_FOLDER_UNIX
+if os.name in ("posix",):
+    icu_data_folder = (
+        ICU_DATA_FOLDER_OSX if sys.platform in ("darwin",) else ICU_DATA_FOLDER_UNIX
+    )
 else:
     icu_data_folder = None
 
-os.environ['PATH'] = f"{os.environ['PATH']}:{DEPOT_HOME}"
+os.environ["PATH"] = f"{os.environ['PATH']}:{DEPOT_HOME}"
 
 gn_args = {
-  "dcheck_always_on"                   : "true" if os.environ.get("STPYV8_DEBUG") else "false",
-  "is_component_build"                 : "false",
-  "is_debug"                           : "true" if os.environ.get("STPYV8_DEBUG") else "false",
-  "treat_warnings_as_errors"           : "false",
-  "use_custom_libcxx"                  : "false",
-  "v8_deprecation_warnings"            : "true",
-  "v8_enable_disassembler"             : "false",
-  "v8_enable_i18n_support"             : "true",
-  "v8_enable_pointer_compression"      : "false",
-  "v8_enable_31bit_smis_on_64bit_arch" : "false",
-  "v8_imminent_deprecation_warnings"   : "true",
-  "v8_monolithic"                      : "true",
-  "v8_use_external_startup_data"       : "false"
+    "dcheck_always_on": "true" if os.environ.get("STPYV8_DEBUG") else "false",
+    "is_component_build": "false",
+    "is_debug": "true" if os.environ.get("STPYV8_DEBUG") else "false",
+    "treat_warnings_as_errors": "false",
+    "use_custom_libcxx": "false",
+    "v8_deprecation_warnings": "true",
+    "v8_enable_disassembler": "false",
+    "v8_enable_i18n_support": "true",
+    "v8_enable_pointer_compression": "false",
+    "v8_enable_31bit_smis_on_64bit_arch": "false",
+    "v8_imminent_deprecation_warnings": "true",
+    "v8_monolithic": "true",
+    "v8_use_external_startup_data": "false",
 }
 
-GN_ARGS = ' '.join(f"{key}={value}" for key, value in gn_args.items())
+GN_ARGS = " ".join(f"{key}={value}" for key, value in gn_args.items())
 
-source_files = ["Exception.cpp",
-                "Platform.cpp",
-                "Isolate.cpp",
-                "Context.cpp",
-                "Engine.cpp",
-                "Wrapper.cpp",
-                "Locker.cpp",
-                "Utils.cpp",
-                "STPyV8.cpp"]
+source_files = [
+    "Exception.cpp",
+    "Platform.cpp",
+    "Isolate.cpp",
+    "Context.cpp",
+    "Engine.cpp",
+    "Wrapper.cpp",
+    "Locker.cpp",
+    "Utils.cpp",
+    "STPyV8.cpp",
+]
 
 
-macros             = [("BOOST_PYTHON_STATIC_LIB", None)]
-include_dirs       = []
-library_dirs       = []
-libraries          = []
-extra_compile_args = []
-extra_link_args    = []
+macros = [("BOOST_PYTHON_STATIC_LIB", None)]
+include_dirs = []
+library_dirs = []
+libraries = []
+extra_compile_args = ["-I%s" % BOOST_ROOT]
+extra_link_args = [
+    "-L/usr/lib/x86_64-linux-gnu",
+    "-l:libstdc++.a",
+    "-L%s" % os.path.join(BOOST_ROOT, "stage", "lib"),
+    "-l:libboost_system.a",
+    "-l:libboost_iostreams.a",
+    "-l:libboost_python37.a",
+    "-l:libv8_monolith.a",
+]
 
-include_dirs.append(os.path.join(V8_HOME, 'include'))
-library_dirs.append(os.path.join(V8_HOME, 'out.gn/x64.release.sample/obj/'))
+include_dirs.append(os.path.join(V8_HOME, "include"))
+library_dirs.append(os.path.join(V8_HOME, "out.gn/x64.release.sample/obj/"))
 
 BOOST_PYTHON_LIB_SHORT = f"boost_python{sys.version_info.major}"
-BOOST_PYTHON_LIB_LONG  = f"boost_python{sys.version_info.major}{sys.version_info.minor}"
+BOOST_PYTHON_LIB_LONG = f"boost_python{sys.version_info.major}{sys.version_info.minor}"
 
 BOOST_PYTHON_UBUNTU_MATRIX = {
-    'default' : BOOST_PYTHON_LIB_LONG,
-    '18.04'   : BOOST_PYTHON_LIB_SHORT,
-    '20.04'   : f"{BOOST_PYTHON_LIB_SHORT}8",
-    '22.04'   : f"{BOOST_PYTHON_LIB_SHORT}10"
+    "default": BOOST_PYTHON_LIB_LONG,
+    "18.04": BOOST_PYTHON_LIB_SHORT,
+    "20.04": f"{BOOST_PYTHON_LIB_SHORT}8",
+    "22.04": f"{BOOST_PYTHON_LIB_SHORT}10",
 }
+
 
 def get_libboost_python_name():
     if not os.path.exists("/etc/lsb-release"):
-        return BOOST_PYTHON_UBUNTU_MATRIX['default']
+        return BOOST_PYTHON_UBUNTU_MATRIX["default"]
 
     platform_info = {}
 
-    with open('/etc/lsb-release', encoding = 'utf-8', mode = 'r') as fd:
+    with open("/etc/lsb-release", encoding="utf-8", mode="r") as fd:
         for line in fd.readlines():
             s = line.strip()
             p = s.split("=")
@@ -91,30 +107,33 @@ def get_libboost_python_name():
 
             platform_info[p[0]] = p[1]
 
-    if 'DISTRIB_ID' not in platform_info:
-        return BOOST_PYTHON_UBUNTU_MATRIX['default']
+    if "DISTRIB_ID" not in platform_info:
+        return BOOST_PYTHON_UBUNTU_MATRIX["default"]
 
-    if platform_info['DISTRIB_ID'].lower() not in ('ubuntu', ):
-        return BOOST_PYTHON_UBUNTU_MATRIX['default']
+    if platform_info["DISTRIB_ID"].lower() not in ("ubuntu",):
+        return BOOST_PYTHON_UBUNTU_MATRIX["default"]
 
-    release = platform_info['DISTRIB_RELEASE']
+    release = platform_info["DISTRIB_RELEASE"]
 
     if release not in BOOST_PYTHON_UBUNTU_MATRIX:
-        return BOOST_PYTHON_UBUNTU_MATRIX['default']
+        return BOOST_PYTHON_UBUNTU_MATRIX["default"]
 
     return BOOST_PYTHON_UBUNTU_MATRIX[release]
 
-STPYV8_BOOST_PYTHON = os.getenv('STPYV8_BOOST_PYTHON', default = get_libboost_python_name())
 
-if os.name in ("nt", ):
-    include_dirs       += os.environ["INCLUDE"].split(';')
-    library_dirs       += os.environ["LIB"].split(';')
-    libraries          += ["winmm", "ws2_32"]
+STPYV8_BOOST_PYTHON = os.getenv(
+    "STPYV8_BOOST_PYTHON", default=get_libboost_python_name()
+)
+
+if os.name in ("nt",):
+    include_dirs += os.environ["INCLUDE"].split(";")
+    library_dirs += os.environ["LIB"].split(";")
+    libraries += ["winmm", "ws2_32"]
     extra_compile_args += ["/O2", "/GL", "/MT", "/EHsc", "/Gy", "/Zi"]
-    extra_link_args    += ["/DLL", "/OPT:REF", "/OPT:ICF", "/MACHINE:X86"]
-elif os.name in ("posix", ):
-    libraries = ["boost_system", "boost_iostreams", "v8_monolith", STPYV8_BOOST_PYTHON]
-    extra_compile_args.append('-std=c++17')
+    extra_link_args += ["/DLL", "/OPT:REF", "/OPT:ICF", "/MACHINE:X86"]
+elif os.name in ("posix",):
+    #libraries = ["boost_system", "boost_iostreams", "v8_monolith", STPYV8_BOOST_PYTHON]
+    extra_compile_args.append("-std=c++17")
 
-    if platform.system() in ('Linux', ):
+    if platform.system() in ("Linux",):
         libraries.append("rt")
--- setup.py
+++ setup.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python
 
-import sys
-import os
-import subprocess
-import shutil
 import logging
+import os
+import shutil
+import subprocess
+import sys
+import tarfile
+import tempfile
+import urllib.request
 
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from setuptools.command.install import install
 
-from settings import * # pylint:disable=wildcard-import,unused-wildcard-import
+from settings import *  # pylint:disable=wildcard-import,unused-wildcard-import
 
 log = logging.getLogger()
 
 
 def exec_cmd(cmdline, *args, **kwargs):
-    msg    = kwargs.get('msg')
-    cwd    = kwargs.get('cwd', '.')
-    output = kwargs.get('output')
+    msg = kwargs.get("msg")
+    cwd = kwargs.get("cwd", ".")
+    output = kwargs.get("output")
 
     if msg:
         print(msg)
 
-    cmdline = ' '.join([cmdline] + list(args))
+    cmdline = " ".join([cmdline] + list(args))
 
-    proc = subprocess.Popen(cmdline, # pylint:disable=consider-using-with
-                            shell  = kwargs.get('shell', True),
-                            cwd    = cwd,
-                            env    = kwargs.get('env'),
-                            stdout = subprocess.PIPE if output else None,
-                            stderr = subprocess.PIPE if output else None)
+    proc = subprocess.Popen(
+        cmdline,  # pylint:disable=consider-using-with
+        shell=kwargs.get("shell", True),
+        cwd=cwd,
+        env=kwargs.get("env"),
+        stdout=subprocess.PIPE if output else None,
+        stderr=subprocess.PIPE if output else None,
+    )
 
     stdout, stderr = proc.communicate()
 
@@ -47,66 +52,114 @@ def exec_cmd(cmdline, *args, **kwargs):
 
 def install_depot():
     if not os.path.exists(DEPOT_HOME):
-        exec_cmd("git clone",
-                 DEPOT_GIT_URL,
-                 DEPOT_HOME,
-                 cwd = os.path.dirname(DEPOT_HOME),
-                 msg = "Cloning depot tools")
+        exec_cmd(
+            "git clone",
+            DEPOT_GIT_URL,
+            DEPOT_HOME,
+            cwd=os.path.dirname(DEPOT_HOME),
+            msg="Cloning depot tools",
+        )
 
         return
 
     # depot_tools updates itself automatically when running gclient tool
-    if os.path.isfile(os.path.join(DEPOT_HOME, 'gclient')):
-        _, stdout, _ = exec_cmd(os.path.join(DEPOT_HOME, 'gclient'), # pylint:disable=unused-variable
-                                "--version",
-                                cwd    = DEPOT_HOME,
-                                output = True,
-                                msg    = "Found depot tools")
+    if os.path.isfile(os.path.join(DEPOT_HOME, "gclient")):
+        _, stdout, _ = exec_cmd(
+            os.path.join(DEPOT_HOME, "gclient"),  # pylint:disable=unused-variable
+            "--version",
+            cwd=DEPOT_HOME,
+            output=True,
+            msg="Found depot tools",
+        )
+
+
+def build_boost():
+    url = (
+        "https://boostorg.jfrog.io/artifactory/main/release/%s/source/boost_%s.tar.gz"
+        % (BOOST_VERSION, BOOST_VERSION.replace(".", "_"))
+    )
+    if not os.path.isdir(BOOST_HOME):
+        os.makedirs(BOOST_HOME)
+    bootstrap_path = os.path.join(
+        BOOST_HOME, "boost_%s" % BOOST_VERSION.replace(".", "_"), "bootstrap.sh"
+    )
+    if not os.path.isfile(bootstrap_path):
+        print(f"Checkout boost from {url}")
+        save_path = tempfile.mkstemp(".tar.gz")[1]
+        with open(save_path, "wb") as fp:
+            rsp = urllib.request.urlopen(url)
+            fp.write(rsp.read())
+        tar = tarfile.open(save_path, "r:gz")
+        tar.extractall(BOOST_HOME)
+
+    exec_cmd("./bootstrap.sh", cwd=os.path.dirname(bootstrap_path), msg="Run boost bootstrap")
+    exec_cmd(
+        "./b2 --with-system --with-iostreams --with-python cxxflags=-fPIC threading=multi",
+        cwd=os.path.dirname(bootstrap_path),
+        msg="Build boost static libraries",
+    )
 
 
 def checkout_v8():
     if not os.path.exists(V8_HOME):
-        exec_cmd(os.path.join(DEPOT_HOME, 'fetch'),
-                 'v8',
-                 cwd = os.path.dirname(V8_HOME),
-                 msg = "Fetching Google V8 code")
+        exec_cmd(
+            os.path.join(DEPOT_HOME, "fetch"),
+            "v8",
+            cwd=os.path.dirname(V8_HOME),
+            msg="Fetching Google V8 code",
+        )
 
-    exec_cmd('git fetch --tags',
-             cwd = V8_HOME,
-             msg = "Fetching the release tag information")
+    exec_cmd(
+        "git fetch --tags", cwd=V8_HOME, msg="Fetching the release tag information"
+    )
 
-    exec_cmd('git checkout',
-             V8_GIT_TAG,
-             cwd = V8_HOME,
-             msg = f"Checkout Google V8 v{V8_GIT_TAG}")
+    exec_cmd(
+        "git checkout", V8_GIT_TAG, cwd=V8_HOME, msg=f"Checkout Google V8 v{V8_GIT_TAG}"
+    )
 
-    exec_cmd(os.path.join(DEPOT_HOME, 'gclient'),
-             'sync',
-             '-D',
-             cwd = os.path.dirname(V8_HOME),
-             msg = "Syncing Google V8 code")
+    exec_cmd(
+        os.path.join(DEPOT_HOME, "gclient"),
+        "sync",
+        "-D",
+        cwd=os.path.dirname(V8_HOME),
+        msg="Syncing Google V8 code",
+    )
 
     # On Linux, install additional dependencies, per
     # https://v8.dev/docs/build step 4
-    if sys.platform in ("linux", "linux2", ) and v8_deps_linux:
-        exec_cmd('./v8/build/install-build-deps.sh',
-                 cwd = os.path.dirname(V8_HOME),
-                 msg = "Installing additional linux dependencies")
+    if (
+        sys.platform
+        in (
+            "linux",
+            "linux2",
+        )
+        and v8_deps_linux
+    ):
+        exec_cmd(
+            "./v8/build/install-build-deps.sh",
+            cwd=os.path.dirname(V8_HOME),
+            msg="Installing additional linux dependencies",
+        )
+
 
 def build_v8():
-    exec_cmd(os.path.join(DEPOT_HOME, 'gn'),
-             f"gen out.gn/x64.release.sample --args='{GN_ARGS}'",
-             cwd = V8_HOME,
-             msg = f"Generate build scripts for V8 (v{V8_GIT_TAG})")
+    exec_cmd(
+        os.path.join(DEPOT_HOME, "gn"),
+        f"gen out.gn/x64.release.sample --args='{GN_ARGS}'",
+        cwd=V8_HOME,
+        msg=f"Generate build scripts for V8 (v{V8_GIT_TAG})",
+    )
 
-    exec_cmd(os.path.join(DEPOT_HOME, 'ninja'),
-             "-C out.gn/x64.release.sample v8_monolith",
-             cwd = V8_HOME,
-             msg = "Build V8 with ninja")
+    exec_cmd(
+        os.path.join(DEPOT_HOME, "ninja"),
+        "-C out.gn/x64.release.sample v8_monolith",
+        cwd=V8_HOME,
+        msg="Build V8 with ninja",
+    )
 
 
 def clean_stpyv8():
-    build_folder = os.path.join(STPYV8_HOME, 'build')
+    build_folder = os.path.join(STPYV8_HOME, "build")
 
     if os.path.exists(os.path.join(build_folder)):
         shutil.rmtree(build_folder)
@@ -118,27 +171,39 @@ def prepare_v8():
         checkout_v8()
         build_v8()
         # clean_stpyv8()
-    except Exception as e: # pylint:disable=broad-except
+    except Exception as e:  # pylint:disable=broad-except
         log.error("Fail to checkout and build v8, %s", str(e))
+
+
+class stpyv8_build_boost(build_ext):
+    def run(self):
+        build_boost()
 
 
 class stpyv8_build(build_ext):
     def run(self):
-        V8_GIT_TAG = V8_GIT_TAG_STABLE # pylint:disable=redefined-outer-name,unused-variable
+        V8_GIT_TAG = (
+            V8_GIT_TAG_STABLE  # pylint:disable=redefined-outer-name,unused-variable
+        )
+        build_boost()
         prepare_v8()
         build_ext.run(self)
 
 
 class stpyv8_develop(build_ext):
     def run(self):
-        V8_GIT_TAG = V8_GIT_TAG_MASTER # pylint:disable=redefined-outer-name,unused-variable
+        V8_GIT_TAG = (
+            V8_GIT_TAG_MASTER  # pylint:disable=redefined-outer-name,unused-variable
+        )
         prepare_v8()
         build_ext.run(self)
 
 
 class stpyv8_install_v8(build_ext):
     def run(self):
-        V8_GIT_TAG = V8_GIT_TAG_MASTER # pylint:disable=redefined-outer-name,unused-variable
+        V8_GIT_TAG = (
+            V8_GIT_TAG_MASTER  # pylint:disable=redefined-outer-name,unused-variable
+        )
         prepare_v8()
 
 
@@ -150,36 +215,40 @@ class stpyv8_build_no_v8(build_ext):
 
 class stpyv8_install(install):
     def run(self):
-        self.skip_build = True # pylint:disable=attribute-defined-outside-init
+        self.skip_build = True  # pylint:disable=attribute-defined-outside-init
 
         if icu_data_folder:
-            os.makedirs(icu_data_folder, exist_ok = True)
-            shutil.copy(os.path.join(V8_HOME, "out.gn/x64.release.sample/icudtl.dat"),
-                        icu_data_folder)
+            os.makedirs(icu_data_folder, exist_ok=True)
+            shutil.copy(
+                os.path.join(V8_HOME, "out.gn/x64.release.sample/icudtl.dat"),
+                icu_data_folder,
+            )
 
         install.run(self)
 
 
-stpyv8 = Extension(name               = "_STPyV8",
-                   sources            = [os.path.join("src", source) for source in source_files],
-                   define_macros      = macros,
-                   include_dirs       = include_dirs,
-                   library_dirs       = library_dirs,
-                   libraries          = libraries,
-                   extra_compile_args = extra_compile_args,
-                   extra_link_args    = extra_link_args,
-                   )
+stpyv8 = Extension(
+    name="_STPyV8",
+    sources=[os.path.join("src", source) for source in source_files],
+    define_macros=macros,
+    include_dirs=include_dirs,
+    library_dirs=library_dirs,
+    libraries=libraries,
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
+)
 
-setup(name         = "stpyv8",
-      version      = STPYV8_VERSION,
-      description  = "Python Wrapper for Google V8 Engine",
-      platforms    = "x86",
-      author       = "Philip Syme, Angelo Dell'Aera",
-      url          = "https://github.com/cloudflare/stpyv8",
-      license      = "Apache License 2.0",
-      py_modules   = ["STPyV8"],
-      ext_modules  = [stpyv8],
-      classifiers  = [
+setup(
+    name="st-pyv8",
+    version=STPYV8_VERSION,
+    description="Python Wrapper for Google V8 Engine",
+    platforms="x86",
+    author="Philip Syme, Angelo Dell'Aera, drunkdream",
+    url="https://github.com/drunkdream/stpyv8",
+    license="Apache License 2.0",
+    py_modules=["STPyV8"],
+    ext_modules=[stpyv8],
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Plugins",
         "Intended Audience :: Developers",
@@ -201,11 +270,13 @@ setup(name         = "stpyv8",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-      ],
-      cmdclass = dict(
-          build_ext = stpyv8_build,
-          develop   = stpyv8_develop,
-          v8        = stpyv8_install_v8,
-          stpyv8    = stpyv8_build_no_v8,
-          install   = stpyv8_install),
+    ],
+    cmdclass=dict(
+        build_ext=stpyv8_build,
+        develop=stpyv8_develop,
+        v8=stpyv8_install_v8,
+        stpyv8=stpyv8_build_no_v8,
+        install=stpyv8_install,
+        boost=stpyv8_build_boost,
+    ),
 )
