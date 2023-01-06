32bc55b3f4693df16ace2df85b9d8c66e674dff8
Angelo Dell'Aera <buffer@olografix.org>
2023-01-06 16:31:01 +0100

    Revert "Disable the object lifecycle tracing support"
    This reverts commit e9ef68357611e1820ce0db379570bf1fe27ccf73.

--- src/Config.h
+++ src/Config.h
@@ -4,7 +4,7 @@
 #define SUPPORT_PROPERTY 1
 
 // Enable the object lifecycle tracing support
-// #define SUPPORT_TRACE_LIFECYCLE 1
+#define SUPPORT_TRACE_LIFECYCLE 1
 
 // ICU data file
 #define ICU_DATA_UNIX "/usr/share/stpyv8/icudtl.dat"
