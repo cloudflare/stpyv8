95b2dec1d859fff8fd839ef008f5c59bc46b95f9
Angelo Dell'Aera <buffer@olografix.org>
2023-01-25 09:57:39 +0100

    [Unit tests] Minor improvement

--- tests/test_Wrapper.py
+++ tests/test_Wrapper.py
@@ -723,7 +723,7 @@ class TestWrapper(unittest.TestCase):
                 var none = null;
             """)
 
-            self.assertEqual(count+1, sys.getrefcount(None))
+            self.assertTrue(count <= sys.getrefcount(None) <= count + 1)
 
             ctxt.eval("""
                 var none = null;
