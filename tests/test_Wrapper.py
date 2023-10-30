23108f5bc8f8152f1eadbe0583c0a5dbb5598523
Angelo Dell'Aera <buffer@olografix.org>
2023-10-30 15:17:51 +0100

    Debug test failure on Windows (Python 3.11)

--- tests/test_Wrapper.py
+++ tests/test_Wrapper.py
@@ -726,12 +726,18 @@ class TestWrapper(unittest.TestCase):
                 var none = null;
             """)
 
+            print("First assert")
+            print(count)
+            print(sys.getrefcount(None))
             self.assertTrue(count <= sys.getrefcount(None) <= count + 1)
 
             ctxt.eval("""
                 var none = null;
             """)
 
+            print("Second assert")
+            print(count)
+            print(sys.getrefcount(None))
             self.assertTrue(count <= sys.getrefcount(None) <= count + 1)
 
             del ctxt
