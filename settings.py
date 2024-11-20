4a6c19cbb78417c114bdc2598f373e5011d9b2cf
Angelo Dell'Aera <buffer@olografix.org>
2024-11-20 19:14:58 +0100

    Upgrade V8 to v13.1.201.9

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ DEPOT_HOME = os.environ.get("DEPOT_HOME", os.path.join(STPYV8_HOME, "depot_tools
 V8_HOME = os.environ.get("V8_HOME", os.path.join(STPYV8_HOME, "v8"))
 
 V8_GIT_URL = "https://chromium.googlesource.com/v8/v8.git"
-V8_GIT_TAG_STABLE = "13.1.201.8"
+V8_GIT_TAG_STABLE = "13.1.201.9"
 V8_GIT_TAG_MASTER = "master"
 V8_GIT_TAG = V8_GIT_TAG_STABLE
 DEPOT_GIT_URL = "https://chromium.googlesource.com/chromium/tools/depot_tools.git"
